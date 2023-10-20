0999312ca728f987f0ffdbebda046f751053eab2
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-10-20 11:50:23 +0200

    done with springboot profile

--- src/main/java/com/alliance/springbootprofiles/Application.java
+++ src/main/java/com/alliance/springbootprofiles/Application.java
@@ -1,11 +1,13 @@
 package com.alliance.springbootprofiles;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.w3c.dom.ls.LSOutput;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -17,9 +19,14 @@ public class Application implements CommandLineRunner {
   @Autowired
   public Animal animal;
 
+
+  @Value("${animal.name}")
+  private String name;
+
   @Override
   public void run(String... args) throws Exception {
     animal.makeSound();
+    System.out.println(name);
   }
 
 
--- src/main/java/com/alliance/springbootprofiles/Dog.java
+++ src/main/java/com/alliance/springbootprofiles/Dog.java
@@ -1,7 +1,5 @@
 package com.alliance.springbootprofiles;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
